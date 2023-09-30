714397fcc007f70745db3ab6ff53b2c1aca32e09
FY-Han <77890381+FY-Han@users.noreply.github.com>
2023-09-30 01:27:55 +0000

    update

--- main.py
+++ main.py
@@ -4,55 +4,64 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 from datetime import datetime, timedelta
+import pytz
 
-# 获取当前日期
-current_date = datetime.now()
+eastern = pytz.timezone('US/Eastern')
+current_date = datetime.now(eastern)
 
-# 获取周日的日期
+# Sunday Date
 sunday = current_date + timedelta((6-current_date.weekday()) % 7)
 
-# 从明天开始到周日为止
+# Tomorrow Till Sunday
 dates = []
 next_date = current_date + timedelta(days=1)
 while next_date <= sunday:
     dates.append(next_date.strftime('%Y-%m-%d'))
     next_date += timedelta(days=1)
 
-
-# 初始化一个空的DataFrame来存储所有菜单
+# Initialize Empty DataFrame
 all_menus = pd.DataFrame()
 
-# 循环遍历每一天的日期
+# Date Iteration
 for date in dates:
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+
     url_lunch = f"https://api.dineoncampus.com/v1/location/6123ff86a9f13a2a48c2a8ed/periods/64ed0969351d53075fd0c844?platform=0&date={date}"
     url_dinner = f"https://api.dineoncampus.com/v1/location/6123ff86a9f13a2a48c2a8ed/periods/64ed0969351d53075fd0c83e?platform=0&date={date}"
     
-    # 获取午餐和晚餐的菜单
-    response_lunch = requests.get(url_lunch).json()
-    time.sleep(5)
-    response_dinner = requests.get(url_dinner).json()
-    time.sleep(5)
-    
+    # Lunch and Dinner Menu
+    response_lunch = requests.get(url_lunch,headers=headers).json()
+    time.sleep(120)
 
     
-    # 提取并存储菜单
+    # Grep Menu and Save Data
     series_lunch_kitchen = pd.Series([i['name'] for i in response_lunch['menu']['periods']['categories'][0]['items']], name="Lunch_Kitchen")
     series_lunch_flame = pd.Series([i['name'] for i in response_lunch['menu']['periods']['categories'][1]['items']], name="Lunch_Flame")
+
+    print(f"{date} lunch obtained")
+
+    response_dinner = requests.get(url_dinner,headers=headers).json()
+    time.sleep(120)
     series_dinner_kitchen = pd.Series([i['name'] for i in response_dinner['menu']['periods']['categories'][0]['items']], name="Dinner_Kitchen")
     series_dinner_flame = pd.Series([i['name'] for i in response_dinner['menu']['periods']['categories'][1]['items']], name="Dinner_Flame")
+
+    print(f"{date} dinner obtained")
     
     df_daily = pd.concat([series_lunch_kitchen, series_lunch_flame, series_dinner_kitchen, series_dinner_flame], axis=1)
-    df_daily['Date'] = date  # 添加日期列
+    df_daily['Date'] = date  # Add Date
     
     all_menus = pd.concat([all_menus,df_daily])
 
-# 将NaN替换为空字符串
+
+# Replace Nan to empty
 all_menus = all_menus.replace(np.nan, '', regex=True)
 
-# 创建 4*2 的子图阵列
+# Create empty subplots
 fig, axs = plt.subplots(4, 2, figsize=(30, 20))
 
-# 遍历每个日期并在子图上绘制表格
+# Iterate Dates and Plot on Subplot
 for ax, (date, df_date) in zip(axs.flatten(), all_menus.groupby('Date')):
     table = ax.table(cellText=df_date.drop(columns='Date').values,
                      colLabels=df_date.columns.drop('Date'),
@@ -63,7 +72,7 @@ for ax, (date, df_date) in zip(axs.flatten(), all_menus.groupby('Date')):
     table.set_fontsize(9)
     table.auto_set_column_width(col=list(range(len(df_date.columns) - 1)))
     
-    # 加粗第一行字体
+    # Bold First Row
     for (i, j), cell in table.get_celld().items():
         if i == 0:
             cell.set_fontsize(10)
@@ -71,19 +80,18 @@ for ax, (date, df_date) in zip(axs.flatten(), all_menus.groupby('Date')):
     
     date_obj = datetime.strptime(date, '%Y-%m-%d')
 
-    # 获取星期几的缩写
+    # Abbreviation for the Day and Add to Date String
     weekday_abbr = date_obj.strftime('%a')
-
-    # 将星期几的缩写添加到日期字符串中
     date_with_weekday = f"{date} ({weekday_abbr})"
     
     ax.set_title(f"Menu for {date_with_weekday}", fontsize=16, fontweight="bold", pad=20)
 
 axs[3,1].axis('off')
-# 在底部中央加一行文本
+
+# Illustration
 fig.text(0.7, 0.2, "Perch Main Menu, by Fengyang Han", ha='center', va='center', fontsize=12, fontweight='bold')
 
-# 调整子图间的间距
+# Width and Heights Between Subplots
 plt.subplots_adjust(hspace=0.2, wspace=0.1)
-plt.savefig("images/Menu-"+ datetime.now().strftime('%Y-%m-%d') +".jpg")
+plt.savefig("images/Menu-"+ datetime.now(eastern).strftime('%Y-%m-%d') +".jpg")
 plt.show()
